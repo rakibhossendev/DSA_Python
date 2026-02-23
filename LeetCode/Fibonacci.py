4ddc8582fe382a28e18fd454963faae9819d782e
Rakib Hoseen <rh01828315879@gmail.com>
2026-02-23 11:56:26 +0600

    Implement Fibonacci function using recursion

---
+++ LeetCode/Fibonacci.py
@@ -0,0 +1,7 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def f(n):
+            if n == 0 or n == 1:
+                return n
+            return f(n-1) + f(n-2)
+        return f(n)
